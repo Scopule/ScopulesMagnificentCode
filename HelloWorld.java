80000b824ff20fd7ed333927152467bca6a771cb
Scopule <glebka.vershinin@gmail.com>
2021-09-28 16:45:07 +0700

    жаву сделал

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld{
+         public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        int a = 2;
+        String b = input.nextLine(); 
+        System.out.println(b);
+     }
+}
